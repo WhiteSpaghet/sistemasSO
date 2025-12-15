dbe3a2d9137363da1b7308ff0f5e3e3363ee1000
WhiteSpaghet <superivasca@gmail.com>
2025-12-15 18:41:35 +0100

    cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
-// frontend/src/App.jsx
-import Router from "./router";
-import RelojSimulado from "./components/RelojSimulado";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div style={{ fontFamily: "sans-serif", padding: "16px" }}>
-      <header style={{ display: "flex", justifyContent: "space-between", marginBottom: 16 }}>
-        <h1>UNIETAXI</h1>
-        <RelojSimulado />
-      </header>
-      <Router />
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
--- frontend/src/api/taxiApi.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/api/taxiApi.js
-const BASE = "http://localhost:5000/api";
-
-export async function getEstadoTaxi(taxiId) {
-  const res = await fetch(`${BASE}/taxi/estado/${taxiId}`);
-  return res.json();
-}
